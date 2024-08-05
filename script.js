ca8f20c40636fdf9f2ed460a5304999ce0c25769
jerin joy <jerin.joy@impelsys.com>
2024-08-05 20:52:50 +0530

    hhg

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     const participants = [
-        { name: 'Tinto', occup: 'Worker', accommodation: 580,food: 350, transportation: 350, alcohol: 400, percentageChange: 320 },
-        { name: 'Ginto', occup: 'Worker', accommodation: 580,food: 350, transportation: 350, alcohol: 400, percentageChange: 320 },
-        { name: 'Aby',   occup: 'Worker', accommodation: 580,food: 350, transportation: 350, alcohol: 400, percentageChange: 320 },
-        { name: 'Raijo', occup: 'Worker', accommodation: 580,food: 350, transportation: 350, alcohol: 0, percentageChange: 720 },
-        { name: 'Vimal', occup: 'Worker', accommodation: 580,food: 350, transportation: 350, alcohol: 400, percentageChange: 320 },
-        { name: 'Jerin', occup: 'Worker', accommodation: 580,food: 350, transportation: 350, alcohol: 400, percentageChange: 320 },
-        { name: 'Jugal', occup: 'Worker', accommodation: 580,food: 350, transportation: 350, alcohol: 400, percentageChange: 320 },
-        { name: 'Ebin', occup: 'Worker', accommodation: 580,food: 350, transportation: 350, alcohol: 0, percentageChange: 720 },
-        { name: 'Tom',   occup: 'Student', accommodation: 580,food: 350, transportation: 350, alcohol: 400, percentageChange: 672 },
-        { name: 'Alet',  occup: 'Student', accommodation: 580,food: 350, transportation: 350, alcohol: 0, percentageChange: 672 },
-        { name: 'Jesto',  occup: 'Student', accommodation: 580,food: 350, transportation: 350, alcohol: 400, percentageChange: 672 },
-        { name: 'Jefin', occup: 'Student', accommodation: 580,food: 350, transportation: 350, alcohol: 400, percentageChange: 672 },
-        { name: 'Melvin',occup: 'Student', accommodation: 580,food: 350, transportation: 350, alcohol: 400, percentageChange: 672 }
+        { name: 'Tinto', occup: 'Worker', accommodation: 580, food: 350, transportation: 350, alcohol: 400, percentageChange: 320 },
+        { name: 'Ginto', occup: 'Worker', accommodation: 580, food: 350, transportation: 350, alcohol: 400, percentageChange: 320 },
+        { name: 'Aby', occup: 'Worker', accommodation: 580, food: 350, transportation: 350, alcohol: 400, percentageChange: 320 },
+        { name: 'Raijo', occup: 'Worker', accommodation: 580, food: 350, transportation: 350, alcohol: 0, percentageChange: 720 },
+        { name: 'Vimal', occup: 'Worker', accommodation: 580, food: 350, transportation: 350, alcohol: 400, percentageChange: 320 },
+        { name: 'Jerin', occup: 'Worker', accommodation: 580, food: 350, transportation: 350, alcohol: 400, percentageChange: 320 },
+        { name: 'Jugal', occup: 'Worker', accommodation: 580, food: 350, transportation: 350, alcohol: 400, percentageChange: 320 },
+        { name: 'Ebin', occup: 'Worker', accommodation: 580, food: 350, transportation: 350, alcohol: 0, percentageChange: 720 },
+        { name: 'Tom', occup: 'Student', accommodation: 580, food: 350, transportation: 350, alcohol: 400, percentageChange: 672 },
+        { name: 'Alet', occup: 'Student', accommodation: 580, food: 350, transportation: 350, alcohol: 0, percentageChange: 672 },
+        { name: 'Jesto', occup: 'Student', accommodation: 580, food: 350, transportation: 350, alcohol: 400, percentageChange: 672 },
+        { name: 'Jefin', occup: 'Student', accommodation: 580, food: 350, transportation: 350, alcohol: 400, percentageChange: 672 },
+        { name: 'Melvin', occup: 'Student', accommodation: 580, food: 350, transportation: 350, alcohol: 400, percentageChange: 672 }
     ];
 
     const tableBody = document.getElementById('participants-table');
@@ -40,15 +40,14 @@ document.addEventListener("DOMContentLoaded", function() {
         percentage.classList.add('hidden-column');
 
         const givenCell = document.createElement('td');
-        givenCell.dataset.index = index;
-        updateGivenAmount(participant, givenCell);
+        givenCell.textContent = `$${updateGivenAmount(participant).toFixed(2)}`;
 
         // Create "Pay Now" button cell
         const payNowCell = document.createElement('td');
         const payNowButton = document.createElement('button');
         payNowButton.textContent = "Pay Now";
         payNowButton.addEventListener('click', () => {
-            const amount = calculateTotalBudget(participant);
+            const amount = updateGivenAmount(participant).toFixed(2);
             const upiLink = `upi://pay?pa=raijopinhero007@okhdfcbank&pn=trip budject money&am=${amount}&cu=INR`;
             window.location.href = upiLink;
         });
@@ -120,19 +119,17 @@ document.addEventListener("DOMContentLoaded", function() {
         }, 0);
     }
 
-    function updateGivenAmount(participants, givenCell) {
-        let totalBudget = calculateTotalBudget(participants);
-        let percentageChange = parseFloat(participants['percentageChange']);
-        var occup = participants['occup'];
-        console.log(occup);
+    function updateGivenAmount(participant) {
+        let totalBudget = calculateTotalBudget(participant);
+        let percentageChange = parseFloat(participant['percentageChange']);
         let adjustedAmount = 0;
-           if(occup == 'Worker'){
-             adjustedAmount = totalBudget + percentageChange;
-           }else{
-             adjustedAmount = totalBudget - percentageChange;
-           }
-           adjustedAmount = parseFloat(adjustedAmount);
-        givenCell.innerHTML = `$${adjustedAmount}`;
+
+        if (participant.occup === 'Worker') {
+            adjustedAmount = totalBudget + percentageChange;
+        } else {
+            adjustedAmount = totalBudget - percentageChange;
+        }
+        return adjustedAmount;
     }
 
     function setSpecificCheckboxesForParticipant(name, checkboxesToSet) {
